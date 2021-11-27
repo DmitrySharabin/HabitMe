fd531195cdcedfde0aa5210e27450336ef650a68
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-27 17:03:59 +0300

    Initial Commit

---
+++ HabitMe/HabitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMeApp.swift
+//  HabitMe
+//
+//  Created by Dmitry Sharabin on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
